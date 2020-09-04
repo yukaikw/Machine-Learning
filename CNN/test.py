3f45526fec8ba5a10713c95a09ab4091a428e4b8
kevin-0211 <48309016+kevin-0211@users.noreply.github.com>
2020-09-04 23:12:04 +0800

    Add files via upload

---
+++ CNN/test.py
@@ -0,0 +1,150 @@
+import os, sys
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+
+def readfile(path, label):
+    # label 是一個 boolean variable，代表需不需要回傳 y 值
+    image_dir = sorted(os.listdir(path))
+    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    y = np.zeros((len(image_dir)), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))
+        x[i, :, :] = cv2.resize(img,(128, 128))
+        if label:
+          y[i] = int(file.split("_")[0])
+    if label:
+      return x, y
+    else:
+      return x
+
+workspace_dir = "./food-11"
+test_x = readfile(os.path.join(workspace_dir, "testing"), False)
+print("Size of Testing data = {}".format(len(test_x)))
+
+# testing 時不需做 data augmentation
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),                                    
+    transforms.ToTensor(),
+])
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None, size=1):
+        self.x = x
+        # label is required to be a LongTensor
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+        self.size = size
+    def __len__(self):
+        return len(self.x)*self.size
+    def __getitem__(self, index): 
+        index %= len(self.x)
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]           
+            return X, Y
+        else:
+            return X
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        # torch.nn.MaxPool2d(kernel_size, stride, padding)
+        # input 維度 [3, 128, 128]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+
+            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
+
+            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+
+            nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
+
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512*4*4, 4096),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(4096, 1000),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(1000, 11)
+        )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
+
+batch_size = 32
+test_set = ImgDataset(test_x, transform=test_transform)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+model_best = torch.load("vgg16.model")
+model_best.eval()
+prediction = []
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        test_pred = model_best(data.cuda())
+        test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
+        for y in test_label:
+            prediction.append(y)
+
+#將結果寫入 csv 檔
+with open("./predict.csv", 'w') as f:
+    f.write('Id,Category\n')
+    for i, y in  enumerate(prediction):
+        f.write('{},{}\n'.format(i, y))
---
+++ CNN/train.py
@@ -0,0 +1,205 @@
+import os, sys
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+
+
+def readfile(path, label):
+    # label 是一個 boolean variable，代表需不需要回傳 y 值
+    image_dir = sorted(os.listdir(path))
+    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    y = np.zeros((len(image_dir)), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))
+        x[i, :, :] = cv2.resize(img,(128, 128))
+        if label:
+          y[i] = int(file.split("_")[0])
+    if label:
+      return x, y
+    else:
+      return x
+
+
+# 分別將 training set、validation set、testing set 用 readfile 函式讀進來
+workspace_dir = "./food-11"
+print("Reading data")
+train_x, train_y = readfile(os.path.join(workspace_dir, "training"), True)
+print("Size of training data = {}".format(len(train_x)))
+val_x, val_y = readfile(os.path.join(workspace_dir, "validation"), True)
+print("Size of validation data = {}".format(len(val_x)))
+
+
+# training 時做 data augmentation
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(), # 隨機將圖片水平翻轉
+    transforms.RandomRotation(15), # 隨機旋轉圖片
+    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
+    transforms.RandomPerspective(),
+    transforms.RandomAffine(15),
+    transforms.ToTensor(), # 將圖片轉成 Tensor，並把數值 normalize 到 [0,1] (data normalization)
+])
+# testing 時不需做 data augmentation
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),                                    
+    transforms.ToTensor(),
+])
+
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None, size=1):
+        self.x = x
+        # label is required to be a LongTensor
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+        self.size = size
+    def __len__(self):
+        return len(self.x)*self.size
+    def __getitem__(self, index): 
+        index %= len(self.x)
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]           
+            return X, Y
+        else:
+            return X
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        # torch.nn.MaxPool2d(kernel_size, stride, padding)
+        # input 維度 [3, 128, 128]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+
+            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
+
+            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+
+            nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
+
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512*4*4, 4096),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(4096, 1000),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(1000, 11)
+        )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
+
+batch_size = 32
+train_val_x = np.concatenate((train_x, val_x), axis=0)
+train_val_y = np.concatenate((train_y, val_y), axis=0)
+train_val_set = ImgDataset(train_val_x, train_val_y, train_transform, 1)
+train_val_loader = DataLoader(train_val_set, batch_size=batch_size, shuffle=True)
+
+model = Classifier().cuda()
+loss = nn.CrossEntropyLoss() # 因為是 classification task，所以 loss 使用 CrossEntropyLoss
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005) # optimizer 使用 Adam
+num_epoch = 250
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+
+    model.train()
+    for i, data in enumerate(train_val_loader):
+        optimizer.zero_grad()
+        train_pred = model(data[0].cuda())
+        batch_loss = loss(train_pred, data[1].cuda())
+        batch_loss.backward()
+        optimizer.step()
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+
+        #將結果 print 出來
+    print('[%03d/%03d] %2.2f sec(s) Train Acc: %3.6f Loss: %3.6f' % \
+      (epoch + 1, num_epoch, time.time()-epoch_start_time, \
+      train_acc/train_val_set.__len__(), train_loss/train_val_set.__len__()))
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9) # optimizer 使用 SGDM
+num_epoch = 50
+
+for epoch in range(num_epoch):
+    epoch_start_time = time.time()
+    train_acc = 0.0
+    train_loss = 0.0
+
+    model.train()
+    for i, data in enumerate(train_val_loader):
+        optimizer.zero_grad()
+        train_pred = model(data[0].cuda())
+        batch_loss = loss(train_pred, data[1].cuda())
+        batch_loss.backward()
+        optimizer.step()
+
+        train_acc += np.sum(np.argmax(train_pred.cpu().data.numpy(), axis=1) == data[1].numpy())
+        train_loss += batch_loss.item()
+
+        #將結果 print 出來
+    print('[%03d/%03d] %2.2f sec(s) Train Acc: %3.6f Loss: %3.6f' % \
+      (epoch + 1, num_epoch, time.time()-epoch_start_time, \
+      train_acc/train_val_set.__len__(), train_loss/train_val_set.__len__()))
+
+torch.save(model, "vgg16.model")
